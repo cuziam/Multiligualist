c51c097175a1f4c47817556d1b3e39d32cda637b
cuziam <yameame320@gmail.com>
2023-10-18 15:10:24 +0900

    view: 1. toggle on/off로 박스를 변환하는 기능 만드는 중(미완)

---
+++ public/scripts/util.js
@@ -0,0 +1,59 @@
+const iconCopy = document.querySelector("#icon-copy");
+const iconHistory = document.querySelector("#icon-history");
+const tooltip = document.querySelector(".tooltip");
+
+const iconToggleOnList = document.querySelectorAll("#icon-toggle-on");
+const iconToggleOffList = document.querySelectorAll("#icon-toggle-off");
+
+//copy이미지를 누르면 부모 요소 중에 box-text 클래스를 찾는다.
+//거기에 담긴 텍스트를 복사한다.
+function copyText() {
+  const boxTextElement =
+    iconCopy.closest("#input-box").querySelector(".box-text") ||
+    iconCopy.closest(".output-box-toggle-on").querySelector(".box-text");
+
+  let copiedText;
+  if (boxTextElement.tagName === "INPUT") {
+    copiedText = boxTextElement.textContent;
+  } else {
+    copiedText = boxTextElement.value;
+  }
+  navigator.clipboard.writeText(copiedText);
+}
+// let tooltipIntervalId = null;
+
+// function startTooltipInterval() {
+//   tooltipIntervalId = setInterval(() => {
+//     // 아이콘 요소의 위치와 크기를 가져옴
+//     const iconRect = iconCopy.getBoundingClientRect();
+
+//     // 작은 설명창의 내용을 설정
+//     tooltip.textContent = iconCopy.dataset.tooltip;
+
+//     // 작은 설명창을 표시하고, 아이콘 요소의 위치에 배치
+//     tooltip.style.display = "block";
+//     tooltip.style.top = `${iconRect.top - tooltip.offsetHeight}px`;
+//     tooltip.style.left = `${
+//       iconRect.left + iconRect.width / 2 - tooltip.offsetWidth / 2
+//     }px`;
+//   }, 100);
+// }
+
+// function stopTooltipInterval() {
+//   clearInterval(tooltipIntervalId);
+//   tooltip.style.display = "none";
+// }
+
+iconCopy.addEventListener("click", copyText);
+// iconCopy.addEventListener("mouseover", startTooltipInterval);
+// iconCopy.addEventListener("mouseout", stopTooltipInterval);
+
+//토글기능
+iconToggleOnList.forEach((iconToggleOn) => {
+  iconToggleOn.addEventListener("click", () => {
+    const outputBoxes = iconToggleOn.closest("#output-boxes");
+    console.log(outputBoxes);
+    iconToggleOn.closest(".output-box-toggle-on").classList.add("hidden");
+    //추가
+  });
+});
