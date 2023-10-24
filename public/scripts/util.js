2b5ed8d18b4d77f5bd59f2f57262178d05f29afb
cuziam <yameame320@gmail.com>
2023-10-24 14:52:58 +0900

    view: 1. copyText 기능 output box에는 적용되지 않던 버그 수정 2. dropdown기능을 구현해봄 하지만 위치가 제대로 적용되지 않고, 메모리 누수, 동시에 드롭다운을 여러 개 사용시 제대로 작동하지 않는 버그를 발견했음

--- public/scripts/util.js
+++ public/scripts/util.js
@@ -1,8 +1,11 @@
-const iconCopy = document.querySelector("#icon-copy");
+const iconCopyList = document.querySelectorAll("#icon-copy");
 const iconHistory = document.querySelector("#icon-history");
 
 const iconLanguageSelectList = document.querySelectorAll(
   ".icon-language-select"
+);
+const iconTranslatorSelectList = document.querySelectorAll(
+  ".icon-translator-select"
 );
 
 let iconToggleOnList = document.querySelectorAll("#icon-toggle-on");
@@ -18,19 +21,38 @@ const outputBoxToggleOn = document
   .querySelector(".output-box-toggle-on")
   .cloneNode(true);
 
-function copyText() {
-  // Copy text to clipboard
-  const boxTextElement =
-    iconCopy.closest("#input-box").querySelector(".box-text") ||
-    iconCopy.closest(".output-box-toggle-on").querySelector(".box-text");
+// zoom window along with screen resolution
+function zoomWindow(window) {
+  let width = window.innerWidth;
 
+  if (width >= 2560 && width < 3840) {
+    // QHD resolution
+    document.body.style.zoom = "150%"; // zoom 150%
+  } else if (width >= 3840) {
+    // 4K resolution
+    document.body.style.zoom = "200%"; // zoom 200%
+  }
+}
+
+function copyText(iconCopy) {
+  // Copy text to clipboard
+  const boxElement =
+    iconCopy.closest("#input-box") || iconCopy.closest(".output-box-toggle-on");
+  const boxTextElement = boxElement.querySelector(".box-text");
+  console.log(boxTextElement);
   let copiedText;
-  if (boxTextElement.tagName === "INPUT") {
-    copiedText = boxTextElement.textContent;
-  } else {
+  if (boxTextElement.tagName === "TEXTAREA") {
     copiedText = boxTextElement.value;
+  } else {
+    copiedText = boxTextElement.textContent;
   }
   navigator.clipboard.writeText(copiedText);
+
+  //show info of copied text
+  const numOfCopiedText = copiedText.length;
+  const sampleText = copiedText.slice(0, 30).concat("...");
+  console.log("copy text: ", sampleText);
+  console.log("copied text length: ", numOfCopiedText);
 }
 
 // Toggle on/off
@@ -94,36 +116,150 @@ const toggleOn = (iconToggleOff) => {
   outputBoxes.replaceChild(newOutputBoxToggleOn, closestOutputBoxToggleOff);
 };
 
-function zoomWindow(window) {
-  let width = window.innerWidth;
+// Window click 이벤트 리스너는 한 번만 추가합니다.
+let isWindowClickListenerActive = false;
 
-  if (width >= 2560 && width < 3840) {
-    // QHD 해상도
-    document.body.style.zoom = "150%"; // 150%로 확대
-  } else if (width >= 3840) {
-    // 4K 해상도
-    document.body.style.zoom = "200%"; // 200%로 확대
+function showLangDropdown(iconLanguageSelect) {
+  console.log("icon-language-select clicked!");
+
+  // Chosen Language
+  const chosenLang =
+    iconLanguageSelect.parentNode.querySelector(".chosen-lang").textContent;
+
+  // Deepcopy of Dropdown Element
+  const dropdownEle = document.querySelector(".lang-dropdown").cloneNode(true);
+
+  // Style for the Chosen Language
+  const chosenLangEle = dropdownEle.querySelector(
+    `[data-lang="${chosenLang}"]`
+  );
+  if (chosenLangEle) {
+    chosenLangEle.style.color = "var(--brand-second-color)";
+    chosenLangEle.style.fontWeight = "bold";
+  }
+
+  // Dropdown Styles
+  dropdownEle.style.display = "flex";
+  dropdownEle.style.position = "absolute";
+
+  // Insert the Dropdown
+  const closestBox =
+    iconLanguageSelect.closest(".output-box-toggle-on") ||
+    iconLanguageSelect.closest("#input-box");
+  closestBox
+    .querySelector(".box-text")
+    .insertAdjacentElement("afterend", dropdownEle);
+
+  // Event Delegation for Dropdown Items
+  dropdownEle.addEventListener("click", (e) => {
+    if (e.target.matches("[data-lang]")) {
+      console.log("child clicked!");
+      iconLanguageSelect
+        .closest(".language-container")
+        .querySelector(".chosen-lang").textContent = e.target.textContent;
+      dropdownEle.remove();
+    }
+  });
+
+  // Click Outside to Remove Dropdown
+  if (!isWindowClickListenerActive) {
+    window.addEventListener("click", (e) => {
+      if (!e.target.matches(".lang-dropdown div, .icon-language-select")) {
+        dropdownEle.remove();
+      }
+    });
+    isWindowClickListenerActive = true; // 이제 이 리스너는 활성화되었음을 표시
   }
 }
 
-function showDropdown(iconLanguageSelect) {}
+// function showLangDropdown(iconLanguageSelect) {
+//   console.log("icon-language-select clicked!");
+//   const choosenLang =
+//     iconLanguageSelect.parentNode.querySelector(".choosen-lang").textContent; //get the language that user choose
+//   const dropdownEle = document.querySelector(".lang-dropdown").cloneNode(true); //get the deepcopy of dropdown element
 
-//event listeners
+//   //find the text equal to the language that user choose and change the style of the text to bold
+//   const choosenLangEle = dropdownEle.querySelector(`#${choosenLang}`);
+//   choosenLangEle.style.color = "var(--brand-second-color)";
+//   choosenLangEle.style.fontWeight = "bold";
+
+//   dropdownEle.style.display = "flex"; //change the style of dropdown to display flex
+//   dropdownEle.style.position = "absolute"; //change the style of dropdown to position absolute
+
+//   //insert the dropdown element to the position of .box-text
+//   const closestBox =
+//     iconLanguageSelect.closest(".output-box-toggle-on") ||
+//     iconLanguageSelect.closest("#input-box");
+//   closestBox
+//     .querySelector(".box-text")
+//     .insertAdjacentElement("afterend", dropdownEle);
+
+//   //add event listener to each language element
+//   dropdownEle.childNodes.forEach((languageEle) => {
+//     //if user click on the language element, the choosen language will be changed
+//     languageEle.addEventListener("click", () => {
+//       console.log("child clicked!");
+//       iconLanguageSelect.parentNode.querySelector(".choosen-lang").textContent =
+//         languageEle.textContent;
+//       //remove the dropdown element
+//       dropdownEle.remove();
+//     });
+//   });
+
+//   //if user click outside the dropdown element, the dropdown element will disappear
+//   window.addEventListener("click", (e) => {
+//     if (
+//       !e.target.matches(".lang-dropdown div") &&
+//       !e.target.matches(".icon-language-select")
+//     ) {
+//       dropdownEle.remove();
+//     }
+//   });
+// }
+
+function showTransDropdown(iconTranslatorSelect) {
+  console.log("icon-translator-select clicked!");
+  const choosenTool = iconTranslatorSelect.parentNode
+    .querySelector(".choosen-tool")
+    .textContent.split(" ")
+    .join("-"); //get the id of choosen tool that use choose
+  console.log(choosenTool);
+  const dropdown = document.querySelector(".tool-dropdown").cloneNode(true); //get the deepcopy of dropdown element
+  //find the text equal to the language that user choose
+  dropdown.querySelector(`#${choosenTool}`).style.color =
+    "var(--brand-second-color)";
+  //change the style of the text to bold
+  dropdown.querySelector(`#${choosenTool}`).style.fontWeight = "bold";
+  dropdown.style.display = "flex"; //change the style of dropdown to display flex
+  dropdown.style.position = "absolute"; //change the style of dropdown to position absolute
+
+  //insert the dropdown element to the position of .box-text
+  iconTranslatorSelect
+    .closest(".output-box-toggle-on")
+    .querySelector(".box-text")
+    .insertAdjacentElement("afterend", dropdown);
+}
+
+//main event listeners
 window.addEventListener("load", () => {
   zoomWindow(window);
 });
 
 iconLanguageSelectList.forEach((iconLanguageSelect) => {
   iconLanguageSelect.addEventListener("click", () => {
-    showDropdown(iconLanguageSelect);
+    showLangDropdown(iconLanguageSelect);
   });
 });
 
-iconCopy.addEventListener("click", copyText);
+iconTranslatorSelectList.forEach((iconTranslatorSelect) => {
+  iconTranslatorSelect.addEventListener("click", () => {
+    showTransDropdown(iconTranslatorSelect);
+  });
+});
 
-iconFuncList.forEach((iconFunc) => {
-  iconFunc.addEventListener("mouseover", () => {
-    console.log("iconFunc mouseover");
+iconCopyList.forEach((iconCopy) => {
+  iconCopy.addEventListener("click", () => {
+    copyText(iconCopy);
   });
 });
 
