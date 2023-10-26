ce8ac9c7231d2475c57277d81816aedc9e96ffbe
cuziam <yameame320@gmail.com>
2023-10-26 17:22:47 +0900

    view: 1. 드롭박스 창이 제대로 발생하지 않는 버그 수정 2. 드롭박스가 한 번에 하나의 창만 띄울 수 있도록 버그 수정

--- public/scripts/util.js
+++ public/scripts/util.js
@@ -119,69 +119,16 @@ const toggleOn = (iconToggleOff) => {
 // Window click 이벤트 리스너는 한 번만 추가합니다.
 let isWindowClickListenerActive = false;
 
-function showLangDropdown(iconLanguageSelect) {
-  console.log("icon-language-select clicked!");
-
-  // Chosen Language
-  const chosenLang =
-    iconLanguageSelect.parentNode.querySelector(".chosen-lang").textContent;
-
-  // Deepcopy of Dropdown Element
-  const dropdownEle = document.querySelector(".lang-dropdown").cloneNode(true);
-
-  // Style for the Chosen Language
-  const chosenLangEle = dropdownEle.querySelector(
-    `[data-lang="${chosenLang}"]`
-  );
-  if (chosenLangEle) {
-    chosenLangEle.style.color = "var(--brand-second-color)";
-    chosenLangEle.style.fontWeight = "bold";
-  }
-
-  // Dropdown Styles
-  dropdownEle.style.display = "flex";
-  dropdownEle.style.position = "absolute";
-
-  // Insert the Dropdown
-  const closestBox =
-    iconLanguageSelect.closest(".output-box-toggle-on") ||
-    iconLanguageSelect.closest("#input-box");
-  closestBox
-    .querySelector(".box-text")
-    .insertAdjacentElement("afterend", dropdownEle);
-
-  // Event Delegation for Dropdown Items
-  dropdownEle.addEventListener("click", (e) => {
-    if (e.target.matches("[data-lang]")) {
-      console.log("child clicked!");
-      iconLanguageSelect
-        .closest(".language-container")
-        .querySelector(".chosen-lang").textContent = e.target.textContent;
-      dropdownEle.remove();
-    }
-  });
-
-  // Click Outside to Remove Dropdown
-  if (!isWindowClickListenerActive) {
-    window.addEventListener("click", (e) => {
-      if (!e.target.matches(".lang-dropdown div, .icon-language-select")) {
-        dropdownEle.remove();
-      }
-    });
-    isWindowClickListenerActive = true; // 이제 이 리스너는 활성화되었음을 표시
-  }
-}
-
 // function showLangDropdown(iconLanguageSelect) {
 //   console.log("icon-language-select clicked!");
-//   const choosenLang =
-//     iconLanguageSelect.parentNode.querySelector(".choosen-lang").textContent; //get the language that user choose
+//   const chosenLang =
+//     iconLanguageSelect.parentNode.querySelector(".chosen-lang").textContent; //get the language that user choose
 //   const dropdownEle = document.querySelector(".lang-dropdown").cloneNode(true); //get the deepcopy of dropdown element
 
 //   //find the text equal to the language that user choose and change the style of the text to bold
-//   const choosenLangEle = dropdownEle.querySelector(`#${choosenLang}`);
-//   choosenLangEle.style.color = "var(--brand-second-color)";
-//   choosenLangEle.style.fontWeight = "bold";
+//   const chosenLangEle = dropdownEle.querySelector(`#${chosenLang}`);
+//   chosenLangEle.style.color = "var(--brand-second-color)";
+//   chosenLangEle.style.fontWeight = "bold";
 
 //   dropdownEle.style.display = "flex"; //change the style of dropdown to display flex
 //   dropdownEle.style.position = "absolute"; //change the style of dropdown to position absolute
@@ -196,17 +143,17 @@ function showLangDropdown(iconLanguageSelect) {
 
 //   //add event listener to each language element
 //   dropdownEle.childNodes.forEach((languageEle) => {
-//     //if user click on the language element, the choosen language will be changed
+//     //if user click on the language element, the chosen language will be changed
 //     languageEle.addEventListener("click", () => {
 //       console.log("child clicked!");
-//       iconLanguageSelect.parentNode.querySelector(".choosen-lang").textContent =
+//       iconLanguageSelect.parentNode.querySelector(".chosen-lang").textContent =
 //         languageEle.textContent;
 //       //remove the dropdown element
 //       dropdownEle.remove();
 //     });
 //   });
 
-//   //if user click outside the dropdown element, the dropdown element will disappear
+//if user click outside the dropdown element, the dropdown element will disappear
 //   window.addEventListener("click", (e) => {
 //     if (
 //       !e.target.matches(".lang-dropdown div") &&
@@ -217,19 +164,86 @@ function showLangDropdown(iconLanguageSelect) {
 //   });
 // }
 
+// Global variable to keep track of the current dropdown
+let currentDropdown = null;
+
+function showLangDropdown(iconLanguageSelect) {
+  console.log("icon-language-select clicked!");
+  // Remove any existing dropdown
+  if (currentDropdown) {
+    currentDropdown.remove();
+  }
+  // Null check for chosen language
+  const choosenLangEle =
+    iconLanguageSelect.parentNode.querySelector(".chosen-lang");
+  if (!choosenLangEle) {
+    console.error("Chosen language element not found");
+    return;
+  }
+  const choosenLang = choosenLangEle.textContent;
+  // Null check for dropdown element
+  const originalDropdown = document.querySelector(".lang-dropdown");
+  if (!originalDropdown) {
+    console.error("Dropdown element not found");
+    return;
+  }
+  const dropdownEle = originalDropdown.cloneNode(true); // Deep copy
+
+  // Null check for chosen language in dropdown
+  const targetLangEle = dropdownEle.querySelector(`#${choosenLang}`);
+  if (!targetLangEle) {
+    console.error("Target language element not found in dropdown");
+    return;
+  }
+  console.log(targetLangEle);
+  // Style modification using class instead of direct style changes
+  targetLangEle.classList.add("highlighted-language");
+
+  // Layout adjustments
+  dropdownEle.style.display = "flex";
+  dropdownEle.style.position = "absolute";
+
+  // Determine insertion point
+  const closestBox =
+    iconLanguageSelect.closest(".output-box-toggle-on") ||
+    iconLanguageSelect.closest("#input-box");
+
+  if (!closestBox) {
+    console.error("No closest box found");
+    return;
+  }
+
+  // Insert dropdown
+  console.log(closestBox.querySelector(".box-text"), dropdownEle);
+  closestBox
+    .querySelector(".box-text")
+    .insertAdjacentElement("afterend", dropdownEle);
+  // Update the current dropdown
+  currentDropdown = dropdownEle;
+
+  // Add event listener to each language element in dropdown
+  dropdownEle.querySelectorAll(".language-option").forEach((languageEle) => {
+    languageEle.addEventListener("click", () => {
+      console.log("child clicked!");
+      choosenLangEle.textContent = languageEle.textContent;
+      dropdownEle.remove();
+    });
+  });
+}
+
 function showTransDropdown(iconTranslatorSelect) {
   console.log("icon-translator-select clicked!");
-  const choosenTool = iconTranslatorSelect.parentNode
-    .querySelector(".choosen-tool")
+  const chosenTool = iconTranslatorSelect.parentNode
+    .querySelector(".chosen-tool")
     .textContent.split(" ")
-    .join("-"); //get the id of choosen tool that use choose
-  console.log(choosenTool);
+    .join("-"); //get the id of chosen tool that use choose
+  console.log(chosenTool);
   const dropdown = document.querySelector(".tool-dropdown").cloneNode(true); //get the deepcopy of dropdown element
   //find the text equal to the language that user choose
-  dropdown.querySelector(`#${choosenTool}`).style.color =
+  dropdown.querySelector(`#${chosenTool}`).style.color =
     "var(--brand-second-color)";
   //change the style of the text to bold
-  dropdown.querySelector(`#${choosenTool}`).style.fontWeight = "bold";
+  dropdown.querySelector(`#${chosenTool}`).style.fontWeight = "bold";
   dropdown.style.display = "flex"; //change the style of dropdown to display flex
   dropdown.style.position = "absolute"; //change the style of dropdown to position absolute
 
