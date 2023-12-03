ce3c318a4a7c065aadba6c1614e3c7607c459c7d
cuziam <yameame320@gmail.com>
2023-12-03 17:19:06 +0900

    sessionMiddleware함수명을 createSession으로 메소드 명 변경, createSession을 /요청 안에 넣음

--- routes/translators.js
+++ routes/translators.js
@@ -11,13 +11,13 @@ const {
   translateClientReq,
   sendEvents,
 } = require("../src/server/get-api-response");
-const { sessionMiddleware } = require("../src/server/session-controller");
+const { createSession } = require("../src/server/session-controller");
 router.use(cookieParser()); //쿠키 파서 미들웨어
-router.use(sessionMiddleware); //세션 미들웨어
 
 router.get("/", async (req, res) => {
   // 세션에 사용량 정보 저장
   if (req.session._id === undefined) {
+    createSession();
     req.session.maxUsage = 20000;
     req.session.usage = 0;
     await req.session.save();
